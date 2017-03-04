ecf79e6d9d738c3c4405d295632a7dd30404cbed
Markus Potthast <potthast@bestit-online.de>
2017-03-04 12:08:04 +0100

    add comment

--- java/src/main/java/de/detecmedia/checkaccountnumber/MainClass.java
+++ java/src/main/java/de/detecmedia/checkaccountnumber/MainClass.java
@@ -4,6 +4,14 @@ package de.detecmedia.checkaccountnumber;
  * Created by potthast on 03.03.17.
  */
 public class MainClass {
+    /**
+     * simple test.
+     *
+     * @param args
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
     public static void main(String[] args)
             throws
             ClassNotFoundException,
